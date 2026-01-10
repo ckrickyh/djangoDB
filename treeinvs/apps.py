2f80566eb99d627ddd2be77c3dbe650950e8daea
ckrickyh <ckricky.h@gmail.com>
2026-01-10 22:21:15 +0800

    treeinvs

--- treeinvs/apps.py
+++ treeinvs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TreeinvConfig(AppConfig):
+class TreeinvsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'treeinv'
+    name = 'treeinvs'
